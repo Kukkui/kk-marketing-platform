8923ef0132604f6c6caf5f1f7a8f6e38d0d82c4f
Punyawee Posri <punyawee@amitysolutions.com>
2025-04-20 16:54:44 +0700

    Fix tailwind

--- apps/frontend/postcss.config.js
+++ apps/frontend/postcss.config.js
@@ -5,11 +5,18 @@ const { join } = require('path');
 //
 // See: https://nx.dev/guides/using-tailwind-css-in-react#step-4:-applying-configuration-to-libraries
 
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {
+//       config: join(__dirname, 'tailwind.config.js'),
+//     },
+//     autoprefixer: {},
+//   },
+// };
+
 module.exports = {
   plugins: {
-    tailwindcss: {
-      config: join(__dirname, 'tailwind.config.js'),
-    },
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
